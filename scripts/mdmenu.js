524e6000c0e1d2fd9badde73a90fec0262cfdd37
Miha Zupan <mihazupan.zupan1@gmail.com>
2018-10-04 11:03:28 +0200

    Fix base href for md

--- scripts/mdmenu.js
+++ scripts/mdmenu.js
@@ -1,6 +1,6 @@
 var headerHtml = `
 <ul id="menu">
-	<div class="homenav"><a href="auxsites/md/mdmain.html"><img src="images/home.png" alt="Markdown" height="50px" ></a></div>
+	<div class="homenav"><a href="auxsites/md/mdmain.html"><img src="auxsites/md/images/home.png" alt="Markdown" height="50px" ></a></div>
 	<li><a href="auxsites/md/example.html">Example</a>
 		<ul class="dropdown">
 			<li><a href="auxsites/md/example/example2.html">Example2</a>
