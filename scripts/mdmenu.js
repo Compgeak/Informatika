2aa46da765d30c91171fa633c0472496cf6ec0eb
Miha Zupan <mihazupan.zupan1@gmail.com>
2018-10-04 20:56:04 +0200

    Update Markdown site

--- scripts/mdmenu.js
+++ scripts/mdmenu.js
@@ -1,14 +1,5 @@
 var headerHtml = `
 <ul id="menu">
-	<div class="homenav"><a href="auxsites/md/mdmain.html"><img src="auxsites/md/images/home.png" alt="Markdown" height="50px" ></a></div>
-	<li><a href="auxsites/md/example.html">Example</a>
-		<ul class="dropdown">
-			<li><a href="auxsites/md/example/example2.html">Example2</a>
-				<ul class="dropdown-content">
-					<li><a href="auxsites/md/example/2a/example2a.html">Example 2a</a></li>
-				</ul>
-			</il>
-		</ul>
-	</li>
-	<li><a href="auxsites/md/about.html">About</a></li>
+	<div class="homenav"><a href="auxsites/md/index.html"><img src="auxsites/md/images/home.png" alt="Markdown" height="50px" ></a></div>
+	<li><a href="auxsites/md/live/index.html">Try it live!</a></li>
 </ul>`;
